4997d197868bd861b3b866fd22c2ace315b61692
Xilosof <pro100temi444@gmail.com>
2021-02-17 12:39:45 +0300

    Create new Room object

--- src/GREhigh.RoomStaffBase/Interfaces/IRoomFactory.cs
+++ src/GREhigh.RoomStaffBase/Interfaces/IRoomFactory.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections.Generic;
+using GREhigh.DomainBase;
+using GREhigh.DomainBase.Interfaces;
 
 namespace GREhigh.RoomStaffBase.Interfaces {
-    public class IRoomFactory {
+    public interface IRoomFactory {
+        public Room CreateRoom(IRoomSearchParams<Room> searchParams);
+        public Room CreateRoomForParty<TEntity>(Party<Room> party, out TEntity rawTransactions)
+            where TEntity : IEnumerable<RawTransaction>;
     }
 }
