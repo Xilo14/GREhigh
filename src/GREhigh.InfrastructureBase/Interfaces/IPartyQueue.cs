c1835fdcc1e93cc2234c87765fc18dd54c229c40
Xilosof <pro100temi444@gmail.com>
2021-02-17 12:20:14 +0300

    Declare methods to use queue to save object party

--- src/GREhigh.InfrastructureBase/Interfaces/IPartyQueue.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IPartyQueue.cs
@@ -1,5 +1,10 @@
+using System.Collections.Generic;
+using GREhigh.DomainBase;
+
 namespace GREhigh.Infrastructure.Interfaces {
     public interface IPartyQueue {
+        public bool Enqueue<TEntity>(Party<TEntity> party) where TEntity : Room;
+        public Party<Room> Dequeue();
 
     }
 }
