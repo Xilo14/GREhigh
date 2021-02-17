46abbb5c26f1a5f83e9e4bf410205be147397ce0
Xilosof <pro100temi444@gmail.com>
2021-02-17 14:04:17 +0300

    Interface which uses to ensure that only one thread has access to the data of a single room

--- src/GREhigh.InfrastructureBase/Interfaces/IRoomSynchronizer.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IRoomSynchronizer.cs
@@ -1,5 +1,8 @@
+using GREhigh.DomainBase;
+
 namespace GREhigh.Infrastructure.Interfaces {
     public interface IRoomSynchronizer {
-
+        public bool TryLock(Room room);
+        public void Free(Room room);
     }
 }
