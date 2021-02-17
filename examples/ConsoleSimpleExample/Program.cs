f4aa2534f8ebb15fd3724370535b2a97a9eec75c
Xilosof <pro100temi444@gmail.com>
2021-02-17 14:36:55 +0300

    a project for simple testing of the cluster operation

---
+++ examples/ConsoleSimpleExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GREhigh;
+using GREhigh.Builders;
+
+namespace ConsoleSimpleExample {
+    class Program {
+        static void Main(string[] args) {
+            var clusterBuilder = new GREhighClusterBuilder();
+
+        }
+    }
+}
