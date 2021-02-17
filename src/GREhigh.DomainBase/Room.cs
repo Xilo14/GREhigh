32c3e4734a392a13b9a3d4b1ec4db8293b5cb4d1
Xilosof <pro100temi444@gmail.com>
2021-02-17 12:36:17 +0300

    Declared Properties which needed for work with schedular and handler

--- src/GREhigh.DomainBase/Room.cs
+++ src/GREhigh.DomainBase/Room.cs
@@ -1,7 +1,21 @@
 using System;
 
 namespace GREhigh.DomainBase {
-    public class Room {
+    public abstract class Room {
+        public object RoomId { get; set; }
+        public object SchedularJobId { get; set; }
+        public bool IsCanStart { get; }
+        public StatusEnum Status { get; set; }
+        public readonly TimeSpan WaitingTimeout = TimeSpan.FromHours(3);
+        public readonly TimeSpan PreparingTime = TimeSpan.FromSeconds(15);
+        public readonly TimeSpan TickInterval = TimeSpan.FromTicks(0);
 
+        public enum StatusEnum {
+            WaitingForUser,
+            PreparingForGame,
+            Finished,
+            InProccess,
+            Canceled
+        }
     }
 }
