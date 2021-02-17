c1c0c7f4569eb45d938779a7e38680b09157c5a7
Xilosof <pro100temi444@gmail.com>
2021-02-17 14:07:34 +0300

    Object which provides method for filling the PartyQueue

--- src/GREhigh/PartyProducer.cs
+++ src/GREhigh/PartyProducer.cs
@@ -1,5 +1,17 @@
+using GREhigh.DomainBase;
+using GREhigh.Infrastructure.Interfaces;
+
 namespace GREhigh {
     public class PartyProducer {
-
+        private readonly IPartyQueue _queue;
+        internal PartyProducer(
+                IPartyQueue queue) {
+            _queue = queue;
+        }
+        public bool TryProduceParty<TRoomEntity>(Party<TRoomEntity> party)
+                where TRoomEntity : Room {
+            //TODO check coins
+            return _queue.Enqueue(party);
+        }
     }
 }
