8cea261756e9521a77211837ac76c4ab7e411020
Xilosof <pro100temi444@gmail.com>
2021-02-17 12:42:29 +0300

    Object wich handle any update and applies it to the room

--- src/GREhigh.RoomStaffBase/Interfaces/IRoomHandler.cs
+++ src/GREhigh.RoomStaffBase/Interfaces/IRoomHandler.cs
@@ -1,6 +1,30 @@
 using System;
+using System.Collections.Generic;
+using GREhigh.DomainBase;
+using GREhigh.DomainBase.Interfaces;
+using GREhigh.Infrastructure.Interfaces;
 
 namespace GREhigh.RoomStaffBase.Interfaces {
-    public class IRoomHandler {
+    public interface IRoomHandler<TRoom> where TRoom : Room {
+        public Room Room { get; set; }
+        public IRandomizer Randomizer { get; set; }
+        public bool IsStateChanged { get; set; }
+        public bool HandleUpdate<TEnumerableRawTransaction>(
+            IUpdateRoom updateRoom,
+            out TEnumerableRawTransaction rawTransactions)
+                where TEnumerableRawTransaction : IEnumerable<RawTransaction>;
+        public bool TryAddParty<TEnumerableRawTransaction>(
+            Party<TRoom> party,
+            out TEnumerableRawTransaction rawTransactions)
+                where TEnumerableRawTransaction : IEnumerable<RawTransaction>;
+        public void Cancel<TEnumerableRawTransaction>(
+            out TEnumerableRawTransaction rawTransactions)
+                where TEnumerableRawTransaction : IEnumerable<RawTransaction>;
+        public void Tick<TEnumerableRawTransaction>(
+            out TEnumerableRawTransaction rawTransactions)
+                where TEnumerableRawTransaction : IEnumerable<RawTransaction>;
+        public void Start<TEnumerableRawTransaction>(
+            out TEnumerableRawTransaction rawTransactions)
+                where TEnumerableRawTransaction : IEnumerable<RawTransaction>;
     }
 }
