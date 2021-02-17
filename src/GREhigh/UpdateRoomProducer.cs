02062045a96cc3257edf7985c621ba3ee5f6f212
Xilosof <pro100temi444@gmail.com>
2021-02-17 14:07:54 +0300

    Object which provides method for filling the UpdateQueue

--- src/GREhigh/UpdateRoomProducer.cs
+++ src/GREhigh/UpdateRoomProducer.cs
@@ -1,5 +1,38 @@
+using GREhigh.DomainBase;
+using GREhigh.DomainBase.Interfaces;
+using GREhigh.Infrastructure.Interfaces;
+
 namespace GREhigh {
     public class UpdateRoomProducer {
-
+        private readonly IUpdateRoomQueue _queue;
+        internal UpdateRoomProducer(IUpdateRoomQueue queue) {
+            _queue = queue;
+        }
+        public bool TryProduceUpdate(IUpdateRoom update) {
+            return _queue.Enqueue(new UpdateQueueRecord() {
+                RecordType = UpdateQueueRecord.RecordTypeEnum.Update,
+                UpdateRoom = update,
+                RoomId = update.RoomId,
+            });
+        }
+        internal void ProduceCancellation(object roomId) {
+            _queue.Enqueue(new UpdateQueueRecord() {
+                RecordType = UpdateQueueRecord.RecordTypeEnum.Cancellation,
+                RoomId = roomId,
+            });
+        }
+        internal void ProduceTick(object roomId) {
+            _queue.Enqueue(new UpdateQueueRecord() {
+                RecordType = UpdateQueueRecord.RecordTypeEnum.Tick,
+                RoomId = roomId,
+            });
+        }
+        internal void ProduceFinishPreparing(object roomId) {
+            _queue.Enqueue(new UpdateQueueRecord() {
+                RecordType = UpdateQueueRecord.RecordTypeEnum.FinishPreparing,
+                RoomId = roomId,
+            });
+        }
     }
 }
+
