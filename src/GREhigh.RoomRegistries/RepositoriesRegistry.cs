e200e7826e7f30502beb544d375963aac2c1f798
Xilosof <pro100temi444@gmail.com>
2021-02-17 14:16:22 +0300

    Concrete implementation of a abstract registry for store a repositories

---
+++ src/GREhigh.RoomRegistries/RepositoriesRegistry.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using GREhigh.DomainBase;
+using GREhigh.Infrastructure.Interfaces;
+using GREhigh.RoomStaffBase.Interfaces;
+using GREhigh.Utility;
+
+namespace GREhigh.RoomRegistries {
+    public class RepositoriesRegistry : AbstractRegistry<IInfrastructureFactory<IRoomRepository<Room>>> {
+        private RepositoriesRegistry() { }
+        public static RepositoriesRegistry Instance {
+            get { return Singleton<RepositoriesRegistry>.Instance; }
+            private set { }
+        }
+    }
+}
