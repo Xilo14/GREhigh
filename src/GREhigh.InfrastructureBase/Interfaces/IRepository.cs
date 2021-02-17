7eb20b0dbd4c66b4d807889a2dea2c675ed1e9b7
Xilosof <pro100temi444@gmail.com>
2021-02-17 12:38:41 +0300

    Generic Interface to abstract from data storage

--- src/GREhigh.InfrastructureBase/Interfaces/IRepository.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IRepository.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Linq.Expressions;
 
 namespace GREhigh.Infrastructure.Interfaces {
-    public interface IRepository<TEntity>
+
+    public interface IRepository { }
+    public interface IRepository<TEntity> : IRepository
         where TEntity : class {
-        IEnumerable<TEntity> Get(
+        public IEnumerable<TEntity> Get(
             Expression<Func<TEntity, bool>> filter = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             string includeProperties = "");
-        TEntity GetByID(object id);
-        void Insert(TEntity entity);
-        void Delete(object id);
-        void Delete(TEntity entityToDelete);
-        void Update(TEntity entityToUpdate);
+        public TEntity GetByID(object id);
+        public void Insert(TEntity entity);
+        public void Insert(IEnumerable<TEntity> entityList);
+        public void Delete(object id);
+        public void Delete(TEntity entityToDelete);
+        public void Update(TEntity entityToUpdate);
     }
 }
