e057899ec8aa1dc5d8329e17f867fac2eda95ad0
Xilosof <pro100temi444@gmail.com>
2021-02-17 12:11:55 +0300

    wrapper to realize singleton pattern with any class

---
+++ src/GREhigh.Utility/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace GREhigh.Utility {
+    public class Singleton<T> where T : class {
+        private static T s_instance;
+
+        protected Singleton() {
+        }
+
+        private static T CreateInstance() {
+            var cInfo = typeof(T).GetConstructor(
+                BindingFlags.Instance | BindingFlags.NonPublic,
+                null,
+                new Type[0],
+                new ParameterModifier[0]);
+
+            return (T)cInfo.Invoke(null);
+        }
+
+        public static T Instance {
+            get {
+                if (s_instance == null) {
+                    s_instance = CreateInstance();
+                }
+
+                return s_instance;
+            }
+        }
+    }
+}
