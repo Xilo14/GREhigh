10dd80a557364353286628513e84514be3803070
Xilosof <pro100temi444@gmail.com>
2021-02-17 12:20:54 +0300

    Declare methods to use queue to save object UpdateQueueRecord

--- src/GREhigh.InfrastructureBase/Interfaces/IUpdateRoomQueue.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IUpdateRoomQueue.cs
@@ -1,5 +1,9 @@
+using GREhigh.DomainBase;
+using GREhigh.DomainBase.Interfaces;
+
 namespace GREhigh.Infrastructure.Interfaces {
     public interface IUpdateRoomQueue {
-
+        public bool Enqueue(UpdateQueueRecord updateRoom);
+        public UpdateQueueRecord Dequeue();
     }
 }
