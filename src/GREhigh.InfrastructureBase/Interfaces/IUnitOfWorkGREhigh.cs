0a92e82445ffe0034389bc23bdc8f03b0f676bc6
Xilosof <pro100temi444@gmail.com>
2021-02-17 14:26:41 +0300

    represents a abstraction from data strorage. Ensure data integrity inside a unit

--- src/GREhigh.InfrastructureBase/Interfaces/IUnitOfWorkGREhigh.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IUnitOfWorkGREhigh.cs
@@ -1,5 +1,15 @@
-namespace GREhigh.Infrastructure.Interfaces {
-    public interface IUnitOfWorkGREhigh {
+using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using GREhigh.DomainBase;
+using GREhigh.RoomRegistries;
 
+namespace GREhigh.Infrastructure.Interfaces {
+    public interface IUnitOfWorkGREhigh : IDisposable {
+        public void Save();
+        public bool GetRepository<TEntity>(out TEntity repository) where TEntity : IRepository;
+        public bool GetRepository<TEntity>(Type typeRoom, out TEntity repository) where TEntity : IRepository;
+        public bool SetRepositoryRegistry<TEntity>(AbstractRegistry<TEntity> repositoryRegistry)
+            where TEntity : class;
     }
 }
