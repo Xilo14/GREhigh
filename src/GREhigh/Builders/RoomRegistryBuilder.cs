884d03207ed5ba0500affd8c2342f5352a3729b4
Xilosof <pro100temi444@gmail.com>
2021-02-17 14:30:25 +0300

    builder to add realized room into registry and use them in cluster

---
+++ src/GREhigh/Builders/RoomRegistryBuilder.cs
@@ -0,0 +1,56 @@
+using System;
+using GREhigh.DomainBase;
+using GREhigh.Infrastructure.Interfaces;
+using GREhigh.RoomRegistries;
+using GREhigh.RoomStaffBase.Interfaces;
+
+namespace GREhigh.Builders {
+    public class RoomRegistryBuilder {
+        private readonly HandlersRegistry _handlersRegistry;
+        private readonly FactoriesRegistry _factoriesRegistry;
+        private readonly RepositoriesRegistry _repositoriesRegistry;
+
+        private IInfrastructureFactory<IRoomRepository<Room>> _repositoryFactory;
+        private IInfrastructureFactory<IRoomHandler<Room>> _handlerFactory;
+        private IRoomFactory _roomFactory;
+        private readonly GREhighClusterBuilder _builder;
+        private readonly Type _roomType;
+
+        internal RoomRegistryBuilder(
+                HandlersRegistry handlersRegistry,
+                FactoriesRegistry factoriesRegistry,
+                RepositoriesRegistry repositoriesRegistry,
+                GREhighClusterBuilder builder,
+                Type roomType) {
+            _handlersRegistry = handlersRegistry;
+            _factoriesRegistry = factoriesRegistry;
+            _repositoriesRegistry = repositoriesRegistry;
+
+            _roomType = roomType;
+
+            _builder = builder;
+        }
+
+        public RoomRegistryBuilder WithRepository(IInfrastructureFactory<IRoomRepository<Room>> factory) {
+            _repositoryFactory = factory;
+            return this;
+        }
+        public RoomRegistryBuilder WithHandler(IInfrastructureFactory<IRoomHandler<Room>> factory) {
+            _handlerFactory = factory;
+            return this;
+        }
+        public RoomRegistryBuilder WithRoomFactory(IRoomFactory factory) {
+            _roomFactory = factory;
+            return this;
+        }
+
+        public GREhighClusterBuilder SaveRoomToRegistry() {
+            _handlersRegistry.AddToRegistry(_roomType, _handlerFactory);
+            _repositoriesRegistry.AddToRegistry(_roomType, _repositoryFactory);
+            _factoriesRegistry.AddToRegistry(_roomType, _roomFactory);
+
+
+            return _builder;
+        }
+    }
+}
