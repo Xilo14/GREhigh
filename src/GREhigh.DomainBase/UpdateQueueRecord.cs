aca131487a469c47d20652d2fc3d3e02569d9735
Xilosof <pro100temi444@gmail.com>
2021-02-17 12:21:35 +0300

    Record which save misc info for every update, tick and any other action with Room

---
+++ src/GREhigh.DomainBase/UpdateQueueRecord.cs
@@ -0,0 +1,17 @@
+using System;
+using GREhigh.DomainBase.Interfaces;
+
+namespace GREhigh.DomainBase {
+    public class UpdateQueueRecord {
+        public object RoomId { get; set; }
+        public Type RoomType { get; set; }
+        public IUpdateRoom UpdateRoom { get; set; }
+        public RecordTypeEnum RecordType { get; set; }
+        public enum RecordTypeEnum {
+            Update,
+            Tick,
+            Cancellation,
+            FinishPreparing,
+        }
+    }
+}
