646e292d1b9ab6f15fe07848b28876c94cc638e0
Xilosof <pro100temi444@gmail.com>
2021-02-17 14:34:47 +0300

    Interface for "cooking" transactions. It means translate raw transactions,which produces by handlers to a transactions, which saves into data storage. In this class u can implement the calculation of the Commission or referral transfers

--- src/GREhigh.InfrastructureBase/Interfaces/ITransactionChef.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/ITransactionChef.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using GREhigh.DomainBase;
+
 namespace GREhigh.Infrastructure.Interfaces {
     public interface ITransactionChef {
-
+        public IEnumerable<Transaction> Cook(IEnumerable<RawTransaction> rawTransactions);
     }
 }
