e593621aa9871c93d8b1229ca4e3d2aedb5f841b
Xilosof <pro100temi444@gmail.com>
2021-02-18 17:11:27 +0300

    remove 'class' generic restriction

--- src/GREhigh.InfrastructureBase/AbstractRegistry.cs
+++ src/GREhigh.InfrastructureBase/AbstractRegistry.cs
@@ -5,10 +5,10 @@ using GREhigh.DomainBase;
 using GREhigh.Infrastructure.Interfaces;
 
 namespace GREhigh.RoomRegistries {
-    public abstract class AbstractRegistry<TEntity> : IRegistry<TEntity> where TEntity : class {
+    public abstract class AbstractRegistry<TEntity> : IRegistry<TEntity> {
         private readonly ConcurrentDictionary<Type, TEntity> _registry = new();
         public TEntity GetForRoom(Type roomType)
-            => _registry.GetValueOrDefault(roomType, null);
+            => _registry.GetValueOrDefault(roomType);
         public TEntity GetForRoom(Room room)
             => GetForRoom(room.GetType());
         public bool AddToRegistry(Type key, TEntity value)
