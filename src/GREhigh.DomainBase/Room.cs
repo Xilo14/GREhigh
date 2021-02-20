434f0561319590cfaf9bd9d47cf875dc4398fe8d
Xilosof <pro100temi444@gmail.com>
2021-02-20 23:19:28 +0300

    Update Room fields - Add object Result; - change all props on virtual; - timings no more readonly - Add RoomId - Add List with all Players

--- src/GREhigh.DomainBase/Room.cs
+++ src/GREhigh.DomainBase/Room.cs
@@ -1,14 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 namespace GREhigh.DomainBase {
     public abstract class Room {
-        public object RoomId { get; set; }
-        public object SchedulerJobId { get; set; }
-        public bool IsCanStart { get; }
-        public StatusEnum Status { get; set; }
-        public readonly TimeSpan WaitingTimeout = TimeSpan.FromHours(3);
-        public readonly TimeSpan PreparingTime = TimeSpan.FromSeconds(15);
-        public readonly TimeSpan TickInterval = TimeSpan.FromTicks(0);
+        public virtual object Result { get; set; }
+        public virtual List<Player> Players { get; set; }
+        public virtual object RoomId { get; set; }
+        public virtual object SchedulerJobId { get; set; }
+        public virtual bool IsCanStart { get; set; }
+        public virtual StatusEnum Status { get; set; }
+        public TimeSpan WaitingTimeout = TimeSpan.FromHours(3);
+        public TimeSpan PreparingTime = TimeSpan.FromSeconds(15);
+        public TimeSpan TickInterval = TimeSpan.FromTicks(0);
 
         public enum StatusEnum {
             WaitingForUser,
