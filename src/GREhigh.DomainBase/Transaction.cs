faa382794030826677dc3c3b4bd973087a47cece
Xilosof <pro100temi444@gmail.com>
2021-02-20 23:12:51 +0300

    Added basic fields to transaction class

--- src/GREhigh.DomainBase/Transaction.cs
+++ src/GREhigh.DomainBase/Transaction.cs
@@ -2,6 +2,15 @@ using System;
 
 namespace GREhigh.DomainBase {
     public class Transaction {
-
+        public Player Player { get; set; }
+        public int Amount { get; set; }
+        public TransactionTypeEnum Type { get; set; }
+        public object AdditionalInformation { get; set; }
+        public enum TransactionTypeEnum {
+            Result,
+            Freeze,
+            Deposit,
+            Transfer
+        }
     }
 }
