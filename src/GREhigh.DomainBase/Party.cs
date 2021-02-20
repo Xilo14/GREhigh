1fd935d3ce5e00fea991081967e37f6f906e2d40
Xilosof <pro100temi444@gmail.com>
2021-02-20 23:21:25 +0300

    Splitted party.

--- src/GREhigh.DomainBase/Party.cs
+++ src/GREhigh.DomainBase/Party.cs
@@ -4,9 +4,14 @@ using GREhigh.DomainBase;
 using GREhigh.DomainBase.Interfaces;
 
 namespace GREhigh.DomainBase {
-    public class Party<TEntity> where TEntity : Room {
-        public Type RoomType { get; } = typeof(TEntity);
+    public class Party {
+        public Type RoomType { get; set; }
         public List<Player> Players { get; set; }
-        public IRoomSearchParams<TEntity> SearchParams { get; set; }
+        public IRoomSearchParams SearchParams { get; set; }
+    }
+    public class Party<TEntity> : Party where TEntity : Room {
+        public Party() {
+            RoomType = typeof(TEntity);
+        }
     }
 }
