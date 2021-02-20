76ace559b4d636857607cea1c4b326e6fc057ca5
Xilosof <pro100temi444@gmail.com>
2021-02-20 23:14:02 +0300

    Update IScheduler. Now he is getting Type of Room

--- src/GREhigh.InfrastructureBase/Interfaces/IScheduler.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IScheduler.cs
@@ -5,9 +5,9 @@ using GREhigh.InfrastructureBase.Interfaces;
 namespace GREhigh.Infrastructure.Interfaces {
     public interface IScheduler {
         public void RemoveJob(object jobId);
-        public object AddJobCancellation(TimeSpan timeout, object roomId);
-        public object AddJobFinishPreparing(TimeSpan timeout, object roomId);
-        public object AddJobTick(TimeSpan timeout, object roomId);
+        public object AddJobCancellation(TimeSpan timeout, object roomId, Type roomType);
+        public object AddJobFinishPreparing(TimeSpan timeout, object roomId, Type roomType);
+        public object AddJobTick(TimeSpan timeout, object roomId, Type roomType);
         public void SetUpdateProducer(IProducer<IUpdateRoom> producer);
         public delegate void JobDelegate(object RoomId);
     }
