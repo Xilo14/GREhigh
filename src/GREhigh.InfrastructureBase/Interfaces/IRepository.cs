ab81c2d9ce76fb74198333992158691075ba99be
Xilosof <pro100temi444@gmail.com>
2021-02-23 13:59:00 +0300

    Now Repository works only with IHaveId

--- src/GREhigh.InfrastructureBase/Interfaces/IRepository.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IRepository.cs
@@ -2,16 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using GREhigh.Utility.Interfaces;
 
 namespace GREhigh.Infrastructure.Interfaces {
 
     public interface IRepository { }
-    public interface IRepository<TEntity> : IRepository {
+    public interface IRepository<TEntity> : IRepository, IEnumerable<TEntity> where TEntity : IHaveId<ulong> {
         public IEnumerable<TEntity> Get(
             Expression<Func<TEntity, bool>> filter = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             string includeProperties = "");
-        public TEntity GetByID(object id);
+        public TEntity GetByID(ulong id);
         public void Insert(TEntity entity);
         public void Insert(IEnumerable<TEntity> entityList);
         public void Delete(object id);
