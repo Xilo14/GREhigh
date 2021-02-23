ddca93f849ed67c3b05c5be138ea03b8aef5462e
Xilosof <pro100temi444@gmail.com>
2021-02-23 14:08:29 +0300

    Added method to interface which check is party can start search

--- src/GREhigh.InfrastructureBase/Interfaces/IPartyQueue.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IPartyQueue.cs
@@ -4,6 +4,7 @@ using GREhigh.DomainBase;
 namespace GREhigh.Infrastructure.Interfaces {
     public interface IPartyQueue {
         public bool Enqueue<TEntity>(TEntity party) where TEntity : Party;
+        public bool IsPartyUniq<TEntity>(TEntity party) where TEntity : Party;
         public Party Dequeue();
 
     }
