2ec7beafceed0ad25c431495cdc3566c3ca8cf0f
Xilosof <pro100temi444@gmail.com>
2021-09-18 14:26:51 +0300

    dev: make double generic

--- src/GREhigh.InfrastructureBase/Interfaces/IUnitOfWorkGREhigh.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IUnitOfWorkGREhigh.cs
@@ -7,9 +7,11 @@ using GREhigh.RoomRegistries;
 namespace GREhigh.Infrastructure.Interfaces {
     public interface IUnitOfWorkGREhigh : IDisposable {
         public void Save();
-        public bool TryGetRoomRepository<T>(Type typeRoom, out T repository) where T : IRepository<Room>;
+        public abstract bool TryGetRoomRepository<T1, T2>(Type typeRoom, out T1 repository)
+            where T1 : IRepository<T2>
+            where T2 : Room;
         public bool SetRepositoryRegistry<T>(T repositoryRegistry)
-            where T : AbstractRegistry<IInfrastructureFactory<IRoomRepository>>;
+            where T : AbstractRegistry<IInfrastructureFactory<IRepository<Room>>>;
         public IRepository<Transaction> GetTransactionsRepository();
         public IRepository<Player> GetPlayerRepository();
     }
