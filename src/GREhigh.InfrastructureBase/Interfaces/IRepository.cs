4095b2389b7248d1cf981770688ff5b86de6735e
Xilosof <pro100temi444@gmail.com>
2021-09-18 13:21:56 +0300

    dev: add out to generic

--- src/GREhigh.InfrastructureBase/Interfaces/IRepository.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IRepository.cs
@@ -5,18 +5,21 @@ using System.Linq.Expressions;
 using GREhigh.Utility.Interfaces;
 
 namespace GREhigh.Infrastructure.Interfaces {
-
-    public interface IRepository { }
-    public interface IRepository<TEntity> : IRepository, IEnumerable<TEntity> where TEntity : IHaveId<ulong> {
-        public IEnumerable<TEntity> Get(
-            Expression<Func<TEntity, bool>> filter = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+    public interface IRepository<out TEntity> : IEnumerable<TEntity>
+        where TEntity : class, IHaveId<ulong> {
+        public IEnumerable<T> Get<T>(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
             string includeProperties = "");
         public TEntity GetByID(ulong id);
-        public void Insert(TEntity entity);
-        public void Insert(IEnumerable<TEntity> entityList);
+        public void Insert<T>(T entity)
+        where T : class, IHaveId<ulong>;
+        public void Insert<T>(IEnumerable<T> entityList)
+        where T : class, IHaveId<ulong>;
         public void Delete(object id);
-        public void Delete(TEntity entityToDelete);
-        public void Update(TEntity entityToUpdate);
+        public void Delete<T>(T entityToDelete)
+        where T : class, IHaveId<ulong>;
+        public void Update<T>(T entityToUpdate)
+        where T : class, IHaveId<ulong>;
     }
 }
