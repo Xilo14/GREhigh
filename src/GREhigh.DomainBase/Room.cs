4e2127f126b49582c4b2572f1ca87e4a2b9cf857
Xilosof <pro100temi444@gmail.com>
2021-09-18 11:20:03 +0300

    dev: add updating LastUpdateDatetime on changing status

--- src/GREhigh.DomainBase/Room.cs
+++ src/GREhigh.DomainBase/Room.cs
@@ -1,15 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using GREhigh.Utility.Interfaces;
 
 namespace GREhigh.DomainBase {
     public abstract class Room : IHaveId<ulong> {
+        public virtual DateTime LastUpdateDateTime { get; set; } = DateTime.Now;
         public virtual object Result { get; set; }
         public virtual List<Player> Players { get; set; }
         public virtual ulong RoomId { get; set; }
         public virtual object SchedulerJobId { get; set; }
         public virtual bool IsCanStart { get; set; }
-        public virtual StatusEnum Status { get; set; }
+        private StatusEnum _status;
+        public virtual StatusEnum Status {
+            get => _status;
+            set {
+                LastUpdateDateTime = DateTime.Now;
+                _status = value;
+            }
+        }
         public ulong Id { get => RoomId; set => RoomId = value; }
 
         public TimeSpan WaitingTimeout = TimeSpan.FromHours(3);
