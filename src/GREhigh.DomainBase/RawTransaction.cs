b2619179541270f5e9144e9e33cd37de3b1abd67
Xilosof <pro100temi444@gmail.com>
2021-09-23 15:11:11 +0300

    dev: add contsructors for rawtransactions

--- src/GREhigh.DomainBase/RawTransaction.cs
+++ src/GREhigh.DomainBase/RawTransaction.cs
@@ -2,6 +2,8 @@ using System;
 
 namespace GREhigh.DomainBase {
     public class RawTransaction : Transaction, IRawTransaction {
-
+        public RawTransaction() { }
+        public RawTransaction(Player player, int amount, TransactionTypeEnum type)
+         => (Player, Amount, Type) = (player, amount, type);
     }
 }
