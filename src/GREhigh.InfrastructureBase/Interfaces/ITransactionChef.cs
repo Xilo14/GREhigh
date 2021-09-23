f237a6b0d9902c282ae665adf75e1d2e27aed238
Xilosof <pro100temi444@gmail.com>
2021-09-23 15:03:45 +0300

    dev: make transactionchef are generic

--- src/GREhigh.InfrastructureBase/Interfaces/ITransactionChef.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/ITransactionChef.cs
@@ -2,8 +2,8 @@ using System.Collections.Generic;
 using GREhigh.DomainBase;
 
 namespace GREhigh.Infrastructure.Interfaces {
-    public interface ITransactionChef {
-        public IEnumerable<Transaction> Cook(IEnumerable<RawTransaction> rawTransactions);
-        public Transaction Cook(RawTransaction rawTransaction);
+    public interface ITransactionChef<out T> where T : Transaction {
+        public IEnumerable<T> Cook(IEnumerable<RawTransaction> rawTransactions);
+        public T Cook(RawTransaction rawTransaction);
     }
 }
