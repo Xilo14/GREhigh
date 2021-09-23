c99d6b2c5a9021862f2ea3f9138a7ec8d2310de2
Xilosof <pro100temi444@gmail.com>
2021-09-23 15:07:43 +0300

    chore: add guard from proxies. IN FUTURE WILL BE DELETED

--- src/GREhigh.DomainBase/UpdateQueueRecord.cs
+++ src/GREhigh.DomainBase/UpdateQueueRecord.cs
@@ -3,8 +3,16 @@ using GREhigh.DomainBase.Interfaces;
 
 namespace GREhigh.DomainBase {
     public class UpdateQueueRecord {
-        public ulong RoomId { get; set; }
-        public Type RoomType { get; set; }
+        public long RoomId { get; set; }
+        public Type _roomType;
+        public Type RoomType {
+            get => _roomType; set {
+                if (value.Namespace == "Castle.Proxies")
+                    _roomType = value.BaseType;
+                else
+                    _roomType = value;
+            }
+        }
         public IUpdateRoom UpdateRoom { get; set; }
         public RecordTypeEnum RecordType { get; set; }
         public enum RecordTypeEnum {
