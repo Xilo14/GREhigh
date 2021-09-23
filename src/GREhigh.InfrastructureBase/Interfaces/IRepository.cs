c98e34c4838eff5f12d5833a43e74d6aa8c1331a
Xilosof <pro100temi444@gmail.com>
2021-09-23 15:04:22 +0300

    chore: update interfaces for using long instead ulong

--- src/GREhigh.InfrastructureBase/Interfaces/IRepository.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IRepository.cs
@@ -6,20 +6,20 @@ using GREhigh.Utility.Interfaces;
 
 namespace GREhigh.Infrastructure.Interfaces {
     public interface IRepository<out TEntity> : IEnumerable<TEntity>
-        where TEntity : class, IHaveId<ulong> {
+        where TEntity : class, IHaveId<long> {
         public IEnumerable<T> Get<T>(
             Expression<Func<T, bool>> filter = null,
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
             string includeProperties = "");
-        public TEntity GetByID(ulong id);
+        public TEntity GetByID(long id);
         public void Insert<T>(T entity)
-        where T : class, IHaveId<ulong>;
+        where T : class, IHaveId<long>;
         public void Insert<T>(IEnumerable<T> entityList)
-        where T : class, IHaveId<ulong>;
+        where T : class, IHaveId<long>;
         public void Delete(object id);
         public void Delete<T>(T entityToDelete)
-        where T : class, IHaveId<ulong>;
+        where T : class, IHaveId<long>;
         public void Update<T>(T entityToUpdate)
-        where T : class, IHaveId<ulong>;
+        where T : class, IHaveId<long>;
     }
 }
--- src/GREhigh.InfrastructureBase/Interfaces/IScheduler.cs
+++ src/GREhigh.InfrastructureBase/Interfaces/IScheduler.cs
@@ -5,9 +5,9 @@ using GREhigh.InfrastructureBase.Interfaces;
 namespace GREhigh.Infrastructure.Interfaces {
     public interface IScheduler {
         public void RemoveJob(object jobId);
-        public object AddJobCancellation(TimeSpan timeout, ulong roomId, Type roomType);
-        public object AddJobFinishPreparing(TimeSpan timeout, ulong roomId, Type roomType);
-        public object AddJobTick(TimeSpan timeout, ulong roomId, Type roomType);
+        public object AddJobCancellation(TimeSpan timeout, long roomId, Type roomType);
+        public object AddJobFinishPreparing(TimeSpan timeout, long roomId, Type roomType);
+        public object AddJobTick(TimeSpan timeout, long roomId, Type roomType);
         public void SetUpdateProducer(IProducer<IUpdateRoom> producer);
     }
 }
